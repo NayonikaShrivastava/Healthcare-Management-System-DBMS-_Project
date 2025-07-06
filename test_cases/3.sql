2037eb9deb7c5cb03d223e0df1abcf7ed350c6eb
Nayonika <92389652+NayonikaShrivastava@users.noreply.github.com>
2025-07-06 17:53:04 +0530

    Add files via upload

---
+++ test_cases/3.sql
@@ -0,0 +1,7 @@
+
+CALL get_prescription_details('777788889999', '2023-01-01');
+
+
+CALL get_prescription_details('444455556666', '2023-02-15');
+
+CALL get_prescription_details('111122223333', '2023-01-01');
